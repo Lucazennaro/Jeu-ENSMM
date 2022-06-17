9a57800f09349c1f827ebd4a991715455b2b4331
Lucazennaro <zennaro.luca.f5@gmail.com>
2022-06-17 13:57:52 +0200

    Merge branch 'main' of https://github.com/Lucazennaro/Jeu-ENSMM
    # Conflicts:
    #	Jeu_ensmm/src/jeu_ensmm/Jeu.java

--- Jeu_ensmm/src/jeu_ensmm/Jeu.java
+++ Jeu_ensmm/src/jeu_ensmm/Jeu.java
@@ -44,8 +44,8 @@ public class Jeu {
        //this.liste.add(joueur);
         //this.liste.add(new Joueur(1, false, "J1",0,0,false,false,false,false,12,0,2));
 
-       // this.joueur = new Joueur(1,false, "J1",0,32,false,false,false,false,12,0,1);
-        this.liste.add(new Objet(2, "J1",150,575,false,false,false,false,12,0,6));    // a enlever
+       //this.joueur = new Joueur(1,false, "J1",0,0,false,false,false,false,12,0,1);
+        //this.liste.add(new Objet(2, "J1",150,575,false,false,false,false,12,0,6));    // a enlever
 //        this.liste.add(new Joueur(1, false, "J1",0,0,false,false,false,false,12,0,1));
         //Joueur J1 = new Joueur(false, "J1",20,20,40,40,false,false,false,false,10,0);
         //liste.add(J1);
